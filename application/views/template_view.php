b236e11370356a4929652860ccfbf95e060a07be
JeK2a <vipjonpc@mail.ru>
2018-04-21 14:08:31 +0300

    JeK2a
    Доработано оформление кода.

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -13,19 +13,19 @@ Modified by VitalySwipe
 -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+		<meta name="description" content=""/>
+		<meta name="keywords" content=""/>
 		<title>JeK-2a</title>
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
-		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" type="text/css" href="/css/style.css" />
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
+		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css"/>
+		<link rel="stylesheet" type="text/css" href="/css/style.css"/>
 		<script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
 		<script type="text/javascript">
 		// return a random integer between 0 and number
 		function random(number) {
 			
-			return Math.floor( Math.random()*(number+1) );
+			return Math.floor(Math.random() * (number + 1));
 		};
 		
 		// show random quote
@@ -35,7 +35,7 @@ Modified by VitalySwipe
 			quotes.hide();
 			
 			var qlen = quotes.length; //document.write( random(qlen-1) );
-			$( '.quote:eq(' + random(qlen-1) + ')' ).show(); //tag:eq(1)
+			$('.quote:eq(' + random(qlen-1) + ')').show(); //tag:eq(1)
 		});
 		</script>
 	</head>
@@ -43,7 +43,7 @@ Modified by VitalySwipe
 		<div id="wrapper">
 			<div id="header">
 				<div id="logo">
-					<a href="/">ОЛОЛОША</span> <span class="cms">TEAM</span></a>
+					<a href="/">JeK-2a</span> <span class="cms">TEAM</span></a>
 				</div>
 				<div id="menu">
 					<ul>
@@ -52,7 +52,7 @@ Modified by VitalySwipe
 						<li><a href="/portfolio">Портфолио</a></li>
 						<li class="last"><a href="/contacts">Контакты</a></li>
 					</ul>
-					<br class="clearfix" />
+					<br class="clearfix"/>
 				</div>
 			</div>
 			<div id="page">
@@ -99,17 +99,17 @@ Modified by VitalySwipe
 						</p>
 						-->
 					</div>
-					<br class="clearfix" />
+					<br class="clearfix"/>
 				</div>
-				<br class="clearfix" />
+				<br class="clearfix"/>
 			</div>
 			<div id="page-bottom">
 				<div id="page-bottom-sidebar">
 					<h3>Наши контакты</h3>
 					<ul class="list">
-						<li class="first">icq: 199199538</li>
-						<li>skypeid: vitalyswipe</li>
-						<li class="last">email: vitalyswipe@gmail.com</li>
+						<li class="first">icq: </li>
+						<li>skypeid: </li>
+						<li class="last">email: jek2ka@gmail.com</li>
 					</ul>
 				</div>
 				<div id="page-bottom-content">
@@ -137,7 +137,7 @@ Modified by VitalySwipe
 Который построил Джек.
 					</p>
 				</div>
-				<br class="clearfix" />
+				<br class="clearfix"/>
 			</div>
 		</div>
 		<div id="footer">
