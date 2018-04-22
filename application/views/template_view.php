0112aad2a47ea86518790ad0a4aea5d1e0e31202
JeK2a <31507511+JeK2a@users.noreply.github.com>
2018-04-22 21:57:34 +0300

    Добавлена вкладка video TouTube

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -5,40 +5,27 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 		<meta name="description" content=""/>
 		<meta name="keywords" content=""/>
-		<title>JeK-2a</title>
+		<title>JeK2a</title>
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
 		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css"/>
 		<link rel="stylesheet" type="text/css" href="/css/style.css"/>
 		<script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
-		<script type="text/javascript">
-		// return a random integer between 0 and number
-		function random(number) {
-			
-			return Math.floor(Math.random() * (number + 1));
-		};
-		
-		// show random quote
-		$(document).ready(function() { 
-
-			var quotes = $('.quote');
-			quotes.hide();
-			
-			var qlen = quotes.length; //document.write( random(qlen-1) );
-			$('.quote:eq(' + random(qlen-1) + ')').show(); //tag:eq(1)
-		});
-		</script>
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="header">
 				<div id="logo">
-					<a href="/">JeK-2a</span> <span class="cms">TEAM</span></a>
+                    <p>
+                        <a href=""><img src="../images/logo.jpg" width="150" height="150" alt="lorem"></a>
+                    </p>
+<!--					<a href="/">JeK2a</span> <span class="cms">Компани</span></a>-->
 				</div>
 				<div id="menu">
 					<ul>
 						<li class="first active"><a href="/">Главная</a></li>
 						<li><a href="/services">Услуги</a></li>
 						<li><a href="/portfolio">Портфолио</a></li>
+                        <li><a href="/video">Видео</a></li>
 						<li class="last"><a href="/contacts">Контакты</a></li>
 					</ul>
 					<br class="clearfix"/>
@@ -47,32 +34,12 @@
 			<div id="page">
 				<div id="sidebar">
 					<div class="side-box">
-						<h3>Случайная цитата</h3>
-						<p align="justify" class="quote">
-						«Сайт, как живой организм, изменяется и развивается.
-						Нельзя сразу написать идеальный вариант и на этом откланяться - это утопия»
-						</p>
-						<p align="justify" class="quote"><!-- &copy; Vitaly Swipe -->
-						«Все должно быть очень просто, как текстовый файл и при этом функционально
-						и тогда пользователи от нас уйдут»
-						</p>
-						<p align="justify" class="quote">
-						«Критика - это когда критик объясняет автору, как сделал бы он, если бы умел»
-						</p>
-						<p align="justify" class="quote"><!-- &copy; Vitaly Swipe -->
-						«Сумасшедшим становиться тот, кто попытался разобраться в этом сумасшедшем мире»
-						</p>
-						<p align="justify" class="quote">
-						«Опытный разработчик знает, какой выбор ведет к поставленной цели, в то время как
-						новичок каждый раз делает шаг в неизвестность»
-						</p>
-					</div>
-					<div class="side-box">
 						<h3>Основное меню</h3>
 						<ul class="list">
 							<li class="first "><a href="/">Главная</a></li>
 							<li><a href="/services">Услуги</a></li>
 							<li><a href="/portfolio">Портфолио</a></li>
+                            <li><a href="/video">Видео</a></li>
 							<li class="last"><a href="/contacts">Контакты</a></li>
 						</ul>
 					</div>
@@ -98,7 +65,7 @@
 			</div>
 		</div>
 		<div id="footer">
-			<a href="/">JeK-2a</a> &copy; 2018</a>
+			<a href="/">JeK2a</a> &copy; 2018</a>
 		</div>
 	</body>
 </html>
